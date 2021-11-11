b14060573bf105f2a9f5597c718d64b8ccc47af6
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-11 09:13:17 +0300

    Добавьте файлы проекта.

---
+++ src/Employee/Employee/Veterinarian.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Employee
+{
+    public class Veterinarian : IEmployee
+    {
+        public string FirstName { get;set; }
+        public string LastName { get;set; }
+        public string AnimalExperiences { get; set; }
+        public void AddAnimalExperience (Animal animal) { }
+        public bool HasAnimalExperience(Animal animal) { }
+        public bool HealAnimal(Animal animal) { }
+    }
+}
