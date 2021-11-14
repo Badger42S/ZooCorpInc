857cd9197ca40766d169b58a464b09f5697764be
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-14 20:18:24 +0300

    feat: zoo app

---
+++ tests/ZooAppTests/ZooAppTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using ZooApps;
+using Zoos;
+
+namespace ZooAppTests
+{
+    public class ZooAppTest
+    {
+        [Fact]
+        public void ShouldBeCreateZooApp()
+        {
+            var zooApp = new ZooApp();
+            Assert.NotNull(zooApp);
+        }
+        [Fact]
+        public void ShouldBeAddZoo()
+        {
+            var zooApp = new ZooApp();
+            var zoo = new Zoo("Toronto");
+            zooApp.AddZoo(zoo);
+            Assert.Equal(zoo, zooApp.ZooList[0]);
+        }
+    }
+}
